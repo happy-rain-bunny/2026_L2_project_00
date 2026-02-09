66ba6aef0d18481a807f76e984086e7df6b0dc7e
happy_rain_bunny <anr60396@masseyhigh.school.nz>
2026-02-10 09:32:53 +1300

    Function to create single line decorated headings

--- C_01_Make_Statement.py
+++ C_01_Make_Statement.py
@@ -1,1 +1,11 @@
 print("🌍🌍🌍Hello world🌍🌍🌍")
+
+#Fuctions go here
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration * 3}{statement}{decoration * 3}")
+
+#Main Routine goes here
+make_statement(statement="Programming is Fun!", decoration="👍")
